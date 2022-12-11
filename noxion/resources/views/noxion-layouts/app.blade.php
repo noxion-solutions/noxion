1bc3a8c88dc00e160df979873adf648e03ba82ed
VenoM9078 <roushan.venom1@gmail.com>
2022-12-11 11:20:19 +0500

    Update app.blade.php

--- noxion/resources/views/noxion-layouts/app.blade.php
+++ noxion/resources/views/noxion-layouts/app.blade.php
@@ -235,7 +235,12 @@
                 <div class="navbar-right">
                     <div class="menu-button">
                         <a href="#" target="_blank">
-                            <div class="btn btn-primary">sign in</div>
+                            <div class="btn btn-primary" style="padding: 5px 20px 5px 20px"><svg
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="white" class="w-6 h-6" style="width: 20px;">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                </svg></div>
                         </a>
                     </div>
                 </div>
